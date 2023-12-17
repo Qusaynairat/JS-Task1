7cfdf31f99a7b46c713cfec9e11de083c7c2b12e
Qusay Nairat <qusaynairat123@yahoo.com>
2023-12-17 22:51:33 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector("div").textContent="Hello";
+document.querySelector("div").classList.add("demo");
+document.querySelector("h1").textContent="Qusay";
+document.querySelector("h1").classList.remove("vip");
